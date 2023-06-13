487f72c22b147e4ed25a58cef2dcdae4e3a59c04
singwithaashish <singwithaashish@gmail.com>
2023-06-14 01:02:26 +0530

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import { TodoProvider } from "./contexts/TodoContext";
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect } from 'react';
 
-function App() {
+function InnerApp() {
   const { user } = useAuth();
-  console.log(user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    } else {
+      navigate("/login");
+    }
+  }, [user, navigate]);
 
   return (
-    <>
-      {user ? (
-        <TodoProvider>
-          <Dashboard />
-        </TodoProvider>
-      ) : (
-        <Login />
-      )}
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to={user ? "/dashboard" : "/login"} replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={
+        <ProtectedRoute>
+          <TodoProvider>
+            <Dashboard />
+          </TodoProvider>
+        </ProtectedRoute>
+      } />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <InnerApp />
+    </Router>
   );
 }
 
